4d399f336e44b6d8048cc21a21b696c1c5606163
Edson Juliano Drosdeck <edson.drosdeck@gmail.com>
2025-01-24 18:34:15 +0000

    Adicionado debianize #1

---
+++ debian/gnome-shell-extension-quick-settings-audio-panel.doc-base.ex
@@ -0,0 +1,20 @@
+Document: gnome-shell-extension-quick-settings-audio-panel
+Title: Debian gnome-shell-extension-quick-settings-audio-panel Manual
+Author: <insert document author here>
+Abstract: This manual describes what gnome-shell-extension-quick-settings-audio-panel is
+ and how it can be used to
+ manage online manuals on Debian systems.
+Section: unknown
+
+Format: debiandoc-sgml
+Files: /usr/share/doc/gnome-shell-extension-quick-settings-audio-panel/gnome-shell-extension-quick-settings-audio-panel.sgml.gz
+
+Format: postscript
+Files: /usr/share/doc/gnome-shell-extension-quick-settings-audio-panel/gnome-shell-extension-quick-settings-audio-panel.ps.gz
+
+Format: text
+Files: /usr/share/doc/gnome-shell-extension-quick-settings-audio-panel/gnome-shell-extension-quick-settings-audio-panel.text.gz
+
+Format: HTML
+Index: /usr/share/doc/gnome-shell-extension-quick-settings-audio-panel/html/index.html
+Files: /usr/share/doc/gnome-shell-extension-quick-settings-audio-panel/html/*.html
